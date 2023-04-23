a58720f10c3a124b17630da7845717951225b41a
Anna Sheremetova <annie131187@gmail.com>
2023-04-23 18:44:30 +0700

    add functions

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,17 @@
 import Cart from './service/Cart';
 import Book from './domain/Book';
 import MusicAlbum from './domain/MusicAlbum';
+import Movie from './domain/Movie';
 
 const cart = new Cart();
 console.log(cart.items);
 
 cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
 cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+cart.add(new Movie(12, 'Мстители', 500, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'приключения'], '137 мин. / 02:17'));
 
+console.log(cart.items);
+console.log(cart.getSum());
+console.log(cart.getSumWithDiscount(0.2));
+console.log(cart.deleteItem(1008));
 console.log(cart.items);
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,13 +1,26 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-    private _items: Buyable[] = [];
+  private _items: Buyable[] = [];
 
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
+  add(item: Buyable): void {
+      this._items.push(item);
+  }
 
-    get items(): Buyable[] {
-        return [...this._items]; 
-    }
+  get items(): Buyable[] {
+      return [...this._items]; 
+  }
+
+  getSum(): number {
+    return this._items.reduce((acc, item) => acc + item.price, 0);
+  }
+
+  getSumWithDiscount(discount: number): number {
+    return this.getSum() - (this.getSum() * discount);
+  }
+
+  deleteItem(id: number): void {
+    const index = this._items.findIndex(item => item.id === id);
+    this._items.splice(index, 1);
+  }
 }
